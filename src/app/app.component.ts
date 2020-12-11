8be1bca81c4e6270f9edb647b952cfe1c38f5294
Eugênio Barros <eugenio.barros.jr@gmail.com>
2020-12-11 07:45:27 -0300

    feat(app): data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  description = 'Leão';
+  url = 'https://i.pinimg.com/736x/19/be/34/19be34a9dac23230ed3d45872227cbda.jpg';
   title = 'alurapic';
 }
